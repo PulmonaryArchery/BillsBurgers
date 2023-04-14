184c5508dd41b36cc66098d0fc3d41b609cc834e
PulmonaryArchery <103186948+PulmonaryArchery@users.noreply.github.com>
2023-04-14 16:53:33 +0100

    Update Meal.java
    Additional formatting

--- Meal.java
+++ Meal.java
@@ -24,6 +24,7 @@ public class Meal {
         System.out.println("Welcome to Bill's Burgers. Please select your burger. We have hamburgers, cheeseburgers, double cheeseburgers or triple cheeseburgers.");
         String burger = scanner.nextLine();
         Burger burgerChoice = new Burger(burger);
+        
         System.out.println("Thank you for selecting a " + burgerChoice.getType() + ". Would you like any additional toppings? We have cheese, bacon or fried onions.");
         String toppingChoice = scanner.nextLine();
         if (toppingChoice.equalsIgnoreCase("cheese") || toppingChoice.equalsIgnoreCase("bacon") || toppingChoice.equalsIgnoreCase("fried onions")) {
@@ -36,14 +37,17 @@ public class Meal {
             System.out.println("You have selected " + toppingChoice + " on your burger.");
             burgerChoice.addTopping(3);
         } else System.out.println("Please enter a valid selection.");
+        
         System.out.println("What drink would you like with your meal? We have Coke, Diet Coke, Fanta or Sprite.");
         String drink = scanner.nextLine();
         System.out.println("What size would would you like your " + drink);
         String drinkSize = scanner.nextLine();
         Drink drinkChoice = new Drink(drink, drinkSize);
+        
         System.out.println("What side would you like with your meal? We have fries, onion rings, sweet potato fries or mozzarella sticks.");
         String side = scanner.nextLine();
         Side sideChoice = new Side(side);
+        
         double burgerPrice = burgerChoice.getBasePrice();
         double drinkPrice = drinkChoice.getPrice();
         double sidePrice = sideChoice.getPrice();
